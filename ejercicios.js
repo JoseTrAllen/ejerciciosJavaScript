58df41d876ffd775aa8983c3e8bbcb698b4ae704
JoseTA <josetrenor@gmail.com>
2023-12-04 13:48:32 +0100

    ejercicio división

---
+++ ejercicios.js
@@ -0,0 +1,14 @@
+
+
+
+let num1 = prompt("Dime un número");
+let num2 = prompt("Dime otro número");
+let resultado = num1 / num2;
+
+if (num1 == 0 || num2 == 0) {
+  alert("Error");
+  
+} else {
+  alert(`${num1} dividido entre ${num2} es igual a ${resultado}`)
+}
+
